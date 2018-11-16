19fee55baa71aa3a8e7388f5d3109f461a633ad1
Błażej Sowa <bsowa123@gmail.com>
2018-11-16 11:59:22 +0100

    first build attempt

--- gst-build/build.sh
+++ gst-build/build.sh
@@ -6,15 +6,24 @@ cd "$(dirname "$0")"
 
 # install required packages
 apt-get update
-apt-get -y install git
+apt-get -y install git python-setuptools
 
 # Clone Cerbero and change to stable branch
 git clone https://github.com/GStreamer/cerbero.git -b 1.14
 
-# cherry-pick necessary commits
+# apply changes from specific commits
 cd cerbero/
 git cherry-pick -n 24c24a82e528ffcea4c1462c24ebc4dc22d13f45 # openssl: set openssl_platform for ARM Linux
 git cherry-pick -n 238067c88ace738e7ec628103473127aa5e85e95 # recipes: new recipe gst-omx
 git cherry-pick -n 02ba4c1a25b5c427b1081177802365478dfc85d8 # gst-omx: Fix typo
 cd ../
 
+# copy config file
+cp lin-rpi.cbc cerbero/config
+
+# bootstrap Cerbero
+cerbero/cerbero-uninstalled -c cerbero/config/lin-rpi.cbc bootstrap --build-tools-only
+
+# build gstreamer
+mkdir -p output
+cerbero/cerbero-uninstalled -c cerbero/config/lin-rpi.cbc package -t gstreamer-1.0 -o output/
