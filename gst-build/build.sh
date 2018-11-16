64af5bb80f69e73a8ce96620e8b2df5d23b6d5a1
Błażej Sowa <bsowa123@gmail.com>
2018-11-16 10:39:40 +0100

    cherry-pick without committing

--- gst-build/build.sh
+++ gst-build/build.sh
@@ -11,14 +11,10 @@ apt-get -y install git
 # Clone Cerbero and change to stable branch
 git clone https://github.com/GStreamer/cerbero.git -b 1.14
 
-# set git identity
-cd cerbero/
-git config user.email "example@example.com"
-git config user.name "Example"
-
 # cherry-pick necessary commits
-git cherry-pick 24c24a82e528ffcea4c1462c24ebc4dc22d13f45 # openssl: set openssl_platform for ARM Linux
-git cherry-pick 238067c88ace738e7ec628103473127aa5e85e95 # recipes: new recipe gst-omx
-git cherry-pick 02ba4c1a25b5c427b1081177802365478dfc85d8 # gst-omx: Fix typo
+cd cerbero/
+git cherry-pick -n 24c24a82e528ffcea4c1462c24ebc4dc22d13f45 # openssl: set openssl_platform for ARM Linux
+git cherry-pick -n 238067c88ace738e7ec628103473127aa5e85e95 # recipes: new recipe gst-omx
+git cherry-pick -n 02ba4c1a25b5c427b1081177802365478dfc85d8 # gst-omx: Fix typo
 cd ../
 
