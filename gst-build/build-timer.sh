708e3ae25da4cef60a62bc3b0b4b97f748b987be
Błażej Sowa <bsowa123@gmail.com>
2018-11-23 10:54:53 +0100

    start from beginning

--- gst-build/build-timer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Runs a build script and spawns background timer process that outputs elapsed time every minute
-# This is used so that travis won't cancel build due to long period without output
-
-cd "$(dirname "$0")"
-./build.sh | ./timer.sh
