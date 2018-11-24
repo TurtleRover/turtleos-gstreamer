78184cde275ee2fda3d3a6e0a85dd557ba862c8c
Błażej Sowa <bsowa123@gmail.com>
2018-11-24 21:04:10 +0100

    Revert "cherry-pick commits"
    This reverts commit ea5f959c231fc4734f1b2e97ae12e5313fb4ebe4.

--- mount.sh
+++ mount.sh
@@ -1,17 +1,12 @@
 #!/bin/bash 
-# Mounts raspbian image root partition inside cerbero build system
+# Mounts raspbian image inside cerbero build system
 
 IMG='raspbian_lite.img'
 MNT='cerbero/config/raspbian_sysroot'
 
 # Attach loopback device
 LOOP_DEV=`losetup -f --show image/${IMG}`
-if [[ $? != 0 ]]; then
-    echo "Error attaching loopback device"
-    exit 1
-else
-    echo "Attached base loopback at: $LOOP_DEV"
-fi
+echo "Attached base loopback at: $LOOP_DEV"
 
 # Fetch and parse partition info
 LOOP=`basename ${LOOP_DEV}`
@@ -19,7 +14,6 @@ SECTOR_SIZE=`cat /sys/block/${LOOP}/queue/hw_sector_size`
 ROOT_START=`fdisk -l $LOOP_DEV | grep ${LOOP_DEV}p2 | awk '{print $2}'`
 echo "Located root partition at sector $ROOT_START (sector size: ${SECTOR_SIZE}B)"
 
-# Mount root partition
 mkdir -p $MNT
 mount image/${IMG} -o loop,offset=$(($ROOT_START*$SECTOR_SIZE)),rw $MNT
 if [[ $? != 0 ]]; then
@@ -28,6 +22,5 @@ else
     echo "Mounted root partition to $MNT"
 fi
 
-# Close loopback device
 losetup -d $LOOP_DEV
 echo "Closed loopback $LOOP_DEV"
